dc45e32cacd9f74de44a468c6c69f4fcede30e56
Stéphanie Blondeau <stephanie.blondeau@laposte.net>
2017-01-20 13:51:54 +0100

    Deuxieme test

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-var expect = require('chai').expect;
-describe('Hello world testing !', function() {
-  it('should be true', function() {
-    expect(true).to.be.true;
-  })
-});
+let summarizeBasket = (prices, products) => {
+  if(!products || !products.length){
+    return {
+      prices :0,
+      countArticles :0,
+      countProducts :0
+    };
+  }else if(products.length === 1) {
+      // Traitement du panier.
+      let price = prices[products[0]];
+
+      return {
+        prices : price,
+        countArticles : 1,
+        countProducts : 1
+      }; // Fin du return
+    }else {
+      return 'TO BE CONTINUED';
+    }// Fin du if
+  }// Fin duu else
+}// Fin de la fonction
+
+module.exports = {
+  summarizeBasket: summarizeBasket
+};
+//
+// myWonderfulFunction(prices, products){
+//   var price = 0;
+//   var countArticles = products.length();
+//   var countProducts = {};
+//
+//   var position = 0;
+//
+//   // Parcourt du panier
+//   foreach(art is products){
+//
+//     // Recherche du produit
+//     if(countProducts.find(art)){
+//
+//     }else{
+//
+//     }
+//     // Récupération du prix du produit
+//     price += products.find(art);
+//   }
+// };
+// // should return
+// { price: 25, countArticles: 10, countProducts: 4 }
---
+++ tests/exercice.mag.panier.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var summarizeBasket = require('../index').summarizeBasket;
+
+let prices = { banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 };
+let products = [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato',
+'potato', 'tomato', 'potato' ];
+
+describe('summarizeBasket', function(){
+  it('Empty basket should return 0 for each value', function(){
+    var result = summarizeBasket({}, []);
+    expect(result.prices).to.be.equal(0);
+    expect(result.countArticles).to.be.equal(0);
+    expect(result.countProducts).to.be.equal(0);
+  })
+
+  it('Basket with only one product should return price of the product', function(){
+    var result = summarizeBasket(prices, ['cucumber']);
+    expect(result.prices).to.be.equal(4);
+    expect(result.countArticles).to.be.equal(1);
+    expect(result.countProducts).to.be.equal(1);
+  })
+})
